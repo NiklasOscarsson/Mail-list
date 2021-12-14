33af0830de5d6c76f16d98ff524224de0fd78276
Niklas <Niklas.Oscarssons@Gmail.com>
2021-12-14 20:49:26 +0100

    v0.2

--- resources/js/reminder.js
+++ resources/js/reminder.js
@@ -11,7 +11,7 @@ const reminder = `
   <p>En vänlig påminnelse om att skriva in hur det går för Lucas H i ES20</p>
   <p>för att enkelt skicka in detta kan ni följa <a href="http://nti-karlstad.duckdns.org/"><b>denna länk</b></a></p>
   <br>
-  <p>Informationen går iväg till VH onsdag kl.17:00 
+  <p>Informationen går iväg till VH fredag kl.17:00 
   <br>
   <hr>
   <p>Mvh</p>
@@ -25,14 +25,13 @@ const reminder = `
 
 `
 
-const mail_list = [
-  // 'asa.granberg@ntig.se', 
+const mail_list = [ 
   'Niklas.Oscarsson@ga.ntig.se', 
   'robert.jonsson@ga.ntig.se', 
-  /* 'robin.brack@ga.ntig.se', 
+  'robin.brack@ga.ntig.se', 
   'Andreas.Fritiofsson@ntig.se', 
   'andreas.karlsson@ga.ntig.se',
-  'hanna.horling@ntig.se' */
+  //'hanna.horling@ntig.se'
 
 ]
 
